d14fd16b8c20f9cc9132e1876043582920b8f110
Paul Ruvolo <paullundyruvolo@gmail.com>
2017-02-10 16:49:46 -0500

    added recording of wheel encoders

--- scripts/collect_benchmarking_dataset.py
+++ scripts/collect_benchmarking_dataset.py
@@ -4,6 +4,7 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from helper_functions import *
 import rospy
+from neato_node.msg import Encoders
 from collections import OrderedDict
 import cPickle as pickle
 from odom_benchmark.cfg import TangoPoseConfig
@@ -19,7 +20,10 @@ class OdometryBenchmarker(object):
         rospy.Subscriber('/STAR_pose', PoseStamped, self.process_groundtruth_pose)
         rospy.Subscriber('/tango_pose', PoseStamped, self.process_tango_pose)
         rospy.Subscriber('/odom', Odometry, self.process_odom)
+        rospy.Subscriber('/encoders', Encoders, self.process_encoders)
+
         self.ground_truth_poses = OrderedDict()
+        self.encoders = OrderedDict()
         self.odom_poses = OrderedDict()
         self.tango_poses = OrderedDict()
 
@@ -36,7 +40,6 @@ class OdometryBenchmarker(object):
         print self.phase_offset
         x += self.pose_correction*cos(theta+self.phase_offset)
         y += self.pose_correction*sin(theta+self.phase_offset)
-        theta = theta + np.pi
 
         self.tango_poses[m.header.stamp.to_sec()] =  x, y, theta
 
@@ -48,11 +51,15 @@ class OdometryBenchmarker(object):
         x, y, theta = convert_pose_to_xy_and_theta(m.pose.pose)
         self.odom_poses[m.header.stamp.to_sec()] = x, y, theta
 
+    def process_encoders(self, m):
+        self.encoders[m.stamp] = m.leftWheel, m.rightWheel
+
     def dump_data(self):
         f = open('odom_validation.pickle', 'w')
         pickle.dump(self.ground_truth_poses, f)
         pickle.dump(self.odom_poses, f)
         pickle.dump(self.tango_poses, f)
+        pickle.dump(self.encoders, f)
         f.close()
 
     def run(self):
